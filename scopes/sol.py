c8e5c71c720ee9e1c6bce34ef0ac0e1c5ba236c7
Poornima Batham <pbatham21@gmail.com>
2025-03-03 22:55:04 +0530

    add scop based function

---
+++ scopes/sol.py
@@ -0,0 +1,96 @@
+# username ="chaiaurcode"
+
+# def func():
+#     # username ="chai"
+#     print(username)     
+#     # ouput - chai
+
+# print(username)    
+# # chaiaurcode 
+# func()
+
+
+# x = 99
+
+# def func2(y):
+#     x=8
+#     z = x+y
+#     return z
+# res = func2(2)
+# print(res)
+
+
+
+# def fun3():
+#      global x
+#      x = 88
+
+# fun3()
+# print(x)
+
+
+
+# def f1():
+#     x = 88
+#     def f2():
+#         print(x)  
+#     return f2() 
+# myres = f1()         
+
+# print(myres)
+
+# def chaicoder(num):
+#     def actual(x):
+#         return x ** num
+#     return actual 
+# f = chaicoder(2)
+# g = chaicoder(3)
+# print(f(3))
+# print(g(3))
+
+# 1 example: -
+# def f():
+
+# 	# local variable
+# 	s = "I love Geeksforgeeks"
+# 	print(s)
+
+
+# Driver code
+# f()
+
+# s ='pp'
+# def f():
+# 	# global s
+# 	# local variable
+# 	s = "I love Geeksforgeeks"
+# 	print("Inside Function:", s)
+
+# # Driver code
+# f()
+# print(s)
+
+
+
+# This function uses global variable s
+# def f():
+# 	print(s)
+
+
+# # Global scope
+# s = "I love Geeksforgeeks"
+# f()
+
+
+# https://www.geeksforgeeks.org/python-scope-of-variables/
+
+# This function uses global variable s
+def f():
+	print(s)
+
+
+# Global scope
+s = "I love chai"
+s = "I love tea"
+
+f()
