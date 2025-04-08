7b0e6a7b7ec129aecfd009f3f92b2f1db30b6cf6
Poornimabatham <pbatham21@gmail.com>
2025-04-08 15:43:56 +0530

    add footer code

---
+++ explorebeauty/frontend/beautyweb/src/component/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+    <div class="footer-column">
+      <h3>About Us</h3>
+      <p>Information about your company or website.</p>
+    </div>
+    <div class="footer-column">
+      <h3>Contact</h3>
+      <p>Email: info@example.com</p>
+      <p>Phone: +91 XXXXXXXXXX</p>
+    </div>
+    <div class="footer-column">
+      <h3>Follow Us</h3>
+      <ul>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Instagram</a></li>
+      </ul>
+    </div>
+  </footer></>
+  )
+}
+
+export default Footer
